9d97dafccefd7d75681d8c8764d8a1297ad27cf4
Daniel Viana <dani_vianaitb@outlook.com>
2023-10-24 16:51:56 -0300

    feat: finished

--- main.py
+++ main.py
@@ -1,4 +1,50 @@
-# parte 1
+# parte_1
 lista_1 = list(range(6, 21))
+# print(lista_1)
 
+
+# parte_2
+ultimo_elemento = lista_1[-1]
+# print(ultimo_elemento)
+
+
+# parte_3
+lista_1[1] = "Kenzie"
+# print(lista_1)
+
+
+# parte_4
+lista_fatia = lista_1[2:5]
+# print(lista_fatia)
+
+
+# parte_5
+lista_1[-1] = "Academy"
+# print(lista_1)
+
+# parte_6
+lista_1.remove("Kenzie")
+lista_1.remove("Academy")
+# print(lista_1)
+
+# parte_7
+lista_2 = sorted(lista_1, reverse=True)
+# print(lista_1)
+# print(lista_2)
+
+# parte_8
+# print(len(lista_1))
+# print(len(lista_2))
+
+# parte_9
+lista_1.pop()
+lista_2.pop()
+# print(lista_1)
+# print(lista_2)
+
+# parte_10
+
+lista_1.clear()
+lista_2.clear()
 print(lista_1)
+print(lista_2)
